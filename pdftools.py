bb2382b392b97ab39447fd8ce149f33c3b8395b0
Walker Aldridge <daldridge@lairdscomputer.com>
2026-08-14 08:51:34 -0500

    PDF tools working page ordered saving

--- pdftools.py
+++ pdftools.py
@@ -8,12 +8,17 @@ reader = None
 # Load PDF
 def load_pdf():
     global reader
-    file_path = filedialog.askopenfilename()
+    file_path = filedialog.askopenfilename(
+        filetypes=[("PDF files", "*.pdf")]
+    )
     if file_path:
         try:
             reader = PdfReader(file_path)
             fields = reader.get_fields() or {}
             info_label.configure(text=f"Read {len(reader.pages)} pages and {len(fields)} fields")
+            for p in range(len(reader.pages)):
+                pages_entry.insert(tk.END, f"{p+1},")
+            pages_entry.delete(len(pages_entry.get())-1,tk.END)
             output_text.configure(state=tk.NORMAL)
             output_text.delete(1.0, tk.END)
             for name, field in fields.items():
@@ -24,7 +29,25 @@ def load_pdf():
 
 # Save PDF
 def save_pdf():
-    pass
+    global reader
+    if reader is None:
+        return
+    file_path = filedialog.asksaveasfilename(
+        filetypes=[("PDF files", "*.pdf")]
+    )
+    if file_path:
+        try:
+            writer = PdfWriter()
+            pages = [int(p) for p in pages_entry.get().split(',')]
+            if len(pages) == 0:
+                pages = [p+1 for p in range(len(reader.pages))]
+            for page in pages:
+                writer.add_page(reader.pages[page-1])
+            with open(file_path, "wb") as f:
+                writer.write(f)
+            info_label.configure(text=f"Wrote {len(pages)} pages")
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to save file: {e}")
 
 # Main window
 main_window = tk.Tk()
@@ -40,7 +63,13 @@ load_button = ttk.Button(controls, text="Load PDF", command=load_pdf)
 load_button.pack(side="left", padx=5, pady=5)
 save_button = ttk.Button(controls, text="Save PDF", command=save_pdf)
 save_button.pack(side="left", padx=5, pady=5)
-info_label = ttk.Label(controls)
+pages_label = ttk.Label(controls, text="Pages:")
+pages_label.pack(side="left", padx=5, pady=5)
+pages_entry = ttk.Entry(controls, width=30)
+pages_entry.pack(side="left", padx=5, pady=5)
+
+info = ttk.Frame(pane, height=30)
+info_label = ttk.Label(info)
 info_label.pack(side="left", padx=5, pady=5)
 
 output = ttk.Frame(pane, height=400)
@@ -48,6 +77,7 @@ output_text = tk.Text(output, state=tk.DISABLED)
 output_text.pack(expand=True, fill=tk.BOTH)
 
 pane.add(controls)
+pane.add(info)
 pane.add(output)
 
 main_window.mainloop()
